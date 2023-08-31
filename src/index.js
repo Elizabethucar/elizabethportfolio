a9616e05ea24f6dbcdd6b5bb9c4d55a98eb065fa
Elizabeth <elizg77@hotmail.com>
2023-08-31 09:40:10 +0200

    added index route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import Home from './components/Home';
 import Resume from './components/Resume';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route index path='*' element={<App />} />
         <Route path='/' element={<Home />} />
         <Route path='/resume' element={<Resume />} />
       </Routes>
